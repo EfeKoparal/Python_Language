493ceec30c002672369bf116ddb6e5aa9f611ff5
Efe Koparal <hesaplarozgurlukicin1@proton.me>
2022-06-25 17:00:57 +0300

    Create hex.py

---
+++ ileri_seviye_sayilar/hex.py
@@ -0,0 +1,12 @@
+"""
+
+Şimdi de 10'luk tabandaki bir sayıyı 16'lık tabana çevirmek için kullanılan hex() fonksiyonuna bakalım.
+
+hex(32) # Sayımız 16'lık tabanda yazıldı.
+'0x20'
+hex(54) # Sayımız 16'lık tabanda yazıldı.
+'0x36'
+hex(1212) # Sayımız 16'lık tabanda yazıldı.
+'0x4bc'
+
+"""
