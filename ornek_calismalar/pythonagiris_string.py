811bd8c6dcc8d96078c0e8cbe5d099f67f24517d
Efe Koparal <hesaplarozgurlukicin1@proton.me>
2022-06-25 17:03:16 +0300

    Create pythonagiris_string.py

---
+++ ornek_calismalar/pythonagiris_string.py
@@ -0,0 +1,52 @@
+print('Ali\'nin evi') # \ kacis karakteri
+
+print("""
+3 tirnakli string :)
+ 
+ ░██████╗░░█████╗░██╗░░░██╗░██████╗░██████╗
+██╔════╝░██╔══██╗██║░░░██║██╔════╝██╔════╝
+██║░░██╗░███████║██║░░░██║╚█████╗░╚█████╗░
+██║░░╚██╗██╔══██║██║░░░██║░╚═══██╗░╚═══██╗
+╚██████╔╝██║░░██║╚██████╔╝██████╔╝██████╔╝
+░╚═════╝░╚═╝░░╚═╝░╚═════╝░╚═════╝░╚═════╝░
+ 
+ 
+ 
+ 
+ 
+                    aaaaaa
+""")
+
+
+print("merhaba \tdunya") #\t tab yazar
+
+
+print("merhaba\ndunya") #\n bosluk yazar
+
+print("merhaba" + "     " + "dunya")
+
+print("merhaba","dunya")
+
+mesaj = "merhaba"
+
+print(mesaj[0:6]) # anlami sudur: 0 dan 5 e kadarki karakterleri yaz. Genelde pythonda sonki karakter yazilmaz ...
+
+print(mesaj[::2]) #ikiser ikiser karakter secti ve yazdirdi
+
+print(mesaj[::-1]) # tersten yazdirdi
+
+print(mesaj.upper()) # karakterleri buyuk yazdirir...
+
+print(len(mesaj))
+
+print(type(len(mesaj)))
+
+print("Merhaba " * 3)
+
+print(" {} hahahah {} ".format("31", "komik degildir")) # format kullanimi
+ahmet = "ahmet"
+emreden = "emreden"
+
+daha = "daha"
+print(f" {ahmet} {emreden} {daha} maldir") # fstring ... :)
+
