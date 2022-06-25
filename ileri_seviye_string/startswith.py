fe3dc2fcb578243a825305e110bb45419a255f2c
Efe Koparal <efekoparal@proton.me>
2022-06-25 19:00:32 +0300

    Create startswith.py

---
+++ ileri_seviye_string/startswith.py
@@ -0,0 +1,13 @@
+startswith() ve endswith()
+startswith(x) : String x ile başlıyorsa True, başlamıyorsa False değeri döndürür.
+
+endswith(x) : String x ile bitiyorsa True, bitmiyorsa False değeri döndürür.
+
+"Python".startswith("py")
+False
+"Python".startswith("Py")
+True
+"Python".endswith("on")
+True
+"Python".endswith("az")
+False
