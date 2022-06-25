c9591ea4e7b332dd918a8b6b34a3d317bfde2ff8
Efe Koparal <efekoparal@proton.me>
2022-06-25 19:06:01 +0300

    Create is_dis_joint.py

---
+++ ileri_seviye_kumeler(sets)/is_dis_joint.py
@@ -0,0 +1,14 @@
+"""
+
+Kümeler ayrık küme mi ? : isdisjoint() metodu
+Bu metod, eğer iki kümenin kesişim kümesi boş ise True, değilse False döner.
+
+küme1 = {1,2,3,10,34,100,-2}
+küme2 = {1,2,23,34,-1}
+küme3 = {30,40,50}
+küme1.isdisjoint(küme2)
+False
+küme1.isdisjoint(küme3)
+True
+
+"""
