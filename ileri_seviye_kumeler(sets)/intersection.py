3d2ff798df8e9956fdbb9c0c98954b9bb058512c
Efe Koparal <efekoparal@proton.me>
2022-06-25 19:05:16 +0300

    Create intersection.py

---
+++ ileri_seviye_kumeler(sets)/intersection.py
@@ -0,0 +1,17 @@
+"""
+Küme kesişimleri : intersection() metodu
+Bu metod iki kümenin kesişimleri bulmamızı sağlar.
+
+küme1 = {1,2,3,10,34,100,-2}
+küme2 = {1,2,23,34,-1}
+küme1.intersection(küme2)
+{1, 2, 34}
+Küme kesişimleri ve güncelleme : intersection_update() metodu
+Bu metod birinci kümeyle ikinci kümenin kesişimlerini bulur ve birinci kümeyi bu kesişime göre günceller.
+
+küme1 = {1,2,3,10,34,100,-2}
+küme2 = {1,2,23,34,-1}
+küme1.intersection_update(küme2)
+küme1
+{1, 2, 34}
+"""
