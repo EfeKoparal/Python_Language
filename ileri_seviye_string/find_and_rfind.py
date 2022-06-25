ec46dc60ca05f38804a2f7c2555793289ff83cf9
Efe Koparal <efekoparal@proton.me>
2022-06-25 19:02:55 +0300

    Create find_and_rfind.py

---
+++ ileri_seviye_string/find_and_rfind.py
@@ -0,0 +1,18 @@
+"""
+
+find() ve rfind()
+find(x) : x değerini baştan itibaren string içinde arar ve bulursa ilk bulduğu indeksi döndürür. Bulamazsa "-1" değerini verir.
+
+rfind(x) : x değerini sondan itibaren string içinde arar ve bulursa ilk bulduğu indeksi döndürür. Bulamazsa "-1" değerini verir.
+
+"araba".find("a")
+0
+"araba".find("s")
+-1
+"araba".rfind("a")
+4
+"araba".rfind("s")
+-1
+İşte stringlerin belli başlı metodları bunlardır. Diğer metodları da ihtiyacınız olduğu durumda şu siteye bakarak öğrenebilirsiniz.
+
+"""
