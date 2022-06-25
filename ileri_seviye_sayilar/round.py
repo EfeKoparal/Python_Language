e75ce8a9292e60a2abcb05295a5f80806e62d589
Efe Koparal <hesaplarozgurlukicin1@proton.me>
2022-06-25 17:01:34 +0300

    Create round.py

---
+++ ileri_seviye_sayilar/round.py
@@ -0,0 +1,15 @@
+"""
+round fonksiyonu
+Sayıları alta veya üste yuvarlar.
+
+round(3.7)
+4
+round(3.2)
+3
+round(3)
+3
+round(3.21329321321323,4) # Ondalıklı sayının 4. hanesine göre yuvarlar
+3.2133
+round(3.21324321321323,4) # Ondalıklı sayının 4. hanesine göre yuvarlar
+3.2132
+"""
