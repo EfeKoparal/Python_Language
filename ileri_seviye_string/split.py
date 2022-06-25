2d4ed8fe0749b7749fdec23d375d63ce46c27017
Efe Koparal <efekoparal@proton.me>
2022-06-25 19:01:13 +0300

    Create split.py

---
+++ ileri_seviye_string/split.py
@@ -0,0 +1,11 @@
+"""
+split()
+split(a) : Verilen bir a değerine göre string parçalara ayrılarak herbir parça listeye atılır.
+
+liste = "Python Programlama Dili".split(" ") # Boşluk karakterine göre ayrıldı.
+print(liste)
+['Python', 'Programlama', 'Dili']
+liste2 = "Python-Php-Java-C-Javascript".split("-")
+print(liste2)
+['Python', 'Php', 'Java', 'C', 'Javascript']
+"""
