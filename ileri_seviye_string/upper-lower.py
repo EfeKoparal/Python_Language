26d7a34ae85dcc1b72323cf92934a534066bfc89
Efe Koparal <efekoparal@proton.me>
2022-06-25 18:59:56 +0300

    Create upper-lower.py

---
+++ ileri_seviye_string/upper-lower.py
@@ -0,0 +1,18 @@
+"""
+
+upper() ve lower()
+upper() metodu stringin tüm karakterlerini büyük harfe çevirir.
+
+lower() metodu stringin tüm karakterlerini küçük harfe çevirir.
+
+"python".upper()
+'PYTHON'
+"PYTHON".lower()
+'python'
+"PythOn".upper()
+'PYTHON'
+"PythOn".lower()
+'python'
+
+
+"""
