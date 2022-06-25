39464130861f9814157f2aaef25021a170b344a4
Efe Koparal <efekoparal@proton.me>
2022-06-25 19:01:38 +0300

    Create join.py

---
+++ ileri_seviye_string/join.py
@@ -0,0 +1,12 @@
+"""
+join()
+Listenin elemanlarını bir string değeriyle birleştirmemizi sağlar.
+
+liste = ["21","02","2014"]
+"/".join(liste)
+'21/02/2014'
+liste = ["T","B","M","M"]
+".".join(liste)
+'T.B.M.M'
+
+"""
