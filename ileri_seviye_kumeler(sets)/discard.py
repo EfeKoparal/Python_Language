8ebf9784f23d9c782d31ce2b15011a4a64aa8338
Efe Koparal <efekoparal@proton.me>
2022-06-25 19:05:35 +0300

    Create discard.py

---
+++ ileri_seviye_kumeler(sets)/discard.py
@@ -0,0 +1,14 @@
+"""
+
+Kümeden Eleman Çıkartmak : discard() metodu
+İçine verilen değeri kümeden çıkartır. Eğer kümede öyle bir değer yoksa, bu metod hiçbir şey yapmaz(Hata vermez).
+
+küme1 = {1,2,3,4,5,6}
+küme1.discard(2)
+küme1
+{1, 3, 4, 5, 6}
+küme1.discard(10)
+küme1
+{1, 3, 4, 5, 6}
+
+"""
