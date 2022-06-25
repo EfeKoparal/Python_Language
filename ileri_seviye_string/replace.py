d5e10e21c803c51414d6dad6da3b66e10817812c
Efe Koparal <efekoparal@proton.me>
2022-06-25 19:00:15 +0300

    Create replace.py

---
+++ ileri_seviye_string/replace.py
@@ -0,0 +1,12 @@
+"""
+replace()
+replace(x,y) : Stringteki x değerlerini y ile değiştirir.
+
+"Herkes ana baba bacı gardaş".replace("a","o")
+'Herkes ono bobo bocı gordoş'
+"Kunduz".replace("duz","zun")
+'Kunzun'
+"Python Programlama Dili".replace(" ","-")
+'Python-Programlama-Dili'
+
+"""
