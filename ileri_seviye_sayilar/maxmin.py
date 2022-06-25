1e62c5e06580ed8f38d9f42262e7f6fd76acd878
Efe Koparal <hesaplarozgurlukicin1@proton.me>
2022-06-25 16:59:05 +0300

    Create maxmin.py

---
+++ ileri_seviye_sayilar/maxmin.py
@@ -0,0 +1,19 @@
+"""
+max ve min fonksiyonu
+max() fonksiyonu verdiğimiz değerlerin en büyüğünü döndürür.
+
+min() fonksiyonu verdiğimiz değerlerin en küçüğünü döndürür.
+
+max(3,4) # İstediğimiz kadar değer verebiliriz.
+4
+max(100,-2,3,4,1,131)  # İstediğimiz kadar değer verebiliriz.
+131
+max([13.2,-4.32,1.2,15.6]) # Sayıları liste şeklinde de verebiliriz.
+15.6
+max((13.2,-4.32,1.2,15.6)) # Sayıları demet şeklinde de verebiliriz.
+15.6
+min(3,4)
+3
+min(100,-2,3,4,1,131)
+-2
+"""
