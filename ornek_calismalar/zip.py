022bda744ff91b5ca3ebd3108e09d327e8fd6e58
Efe Koparal <hesaplarozgurlukicin1@proton.me>
2022-06-25 17:02:28 +0300

    Create zip.py

---
+++ ornek_calismalar/zip.py
@@ -0,0 +1,28 @@
+"""
+
+
+Problem 4
+Elinizde isimlerin ve soyisimlerin bulunduğu iki tane liste olsun.
+
+        isimler -----> ["Kerim","Tarık","Ezgi","Kemal","İlkay","Şükran","Merve"]
+
+        soyisimler -----> ["Yılmaz","Öztürk","Dağdeviren","Atatürk","Dikmen","Kaya","Polat"]
+
+Bu isimleri ve soyisimleri sırasıyla eşleştirin ve ekrana alt alta isimleri ve soyisimleri yazdırın.
+
+Not: zip() fonksiyonunu kullanmaya çalışın.
+
+
+
+"""
+
+liste1 = ["Kerim","Tarık","Ezgi","Kemal","İlkay","Şükran","Merve"]
+liste2 = ["Yılmaz","Öztürk","Dağdeviren","Atatürk","Dikmen","Kaya","Polat"]
+
+cikti = list(zip(liste1,liste2))
+
+print(cikti)
+
+for i,j in cikti:
+
+    print (i,j)
