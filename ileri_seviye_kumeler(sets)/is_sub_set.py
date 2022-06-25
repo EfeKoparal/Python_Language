6500895f4be07c7f8a2c893612ab4b4c3dd3cb26
Efe Koparal <efekoparal@proton.me>
2022-06-25 19:06:27 +0300

    Create is_sub_set.py

---
+++ ileri_seviye_kumeler(sets)/is_sub_set.py
@@ -0,0 +1,14 @@
+"""
+
+Alt kümesi mi ? : issubset() metodu
+Bu metod , birinci küme ikinci kümenin alt kümesiyse True, değilse False döner.
+
+küme1 = {1,2,3}
+küme2 = {1,2,3,4}
+küme3 = {5,6,7}
+küme1.issubset(küme2)
+True
+küme1.issubset(küme3)
+False
+
+"""
