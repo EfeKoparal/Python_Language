25bfa8d9a0d4590d9c91995094cb070d0ad95285
Efe Koparal <hesaplarozgurlukicin1@proton.me>
2022-06-25 16:57:44 +0300

    Create bin.py

---
+++ ileri_seviye_sayilar/bin.py
@@ -0,0 +1,28 @@
+"""
+
+
+
+İleri Seviye Sayılar
+Bu konuda sayı veritiplerini biraz daha derinlemesine incelemeye ve sayılar üzerinde uygulanabilen yararlı fonksiyonları öğrenmeye çalışacağız.
+
+Bazen programlamada sayılarımızı 10'luk taban haricinde 2'lik(binary) ve 16'lık(hexadecimal) tabanda göstermek ve kullanmak isteyebiliriz.
+
+10'luk tabandaki sayıları 2'lik ve 16'lık tabanda göstermeye başlamadan önce şu linklerden 2'lik(binary) ve 16'lık(hexadecimal) tabanları incelemenizi tavsiye ediyorum.
+
+https://www.turkcebilgi.com/ikili_(binary)_say%C4%B1_sistemleri
+
+http://www.blogkafem.net/2014/01/sayi-sistemleri.html
+
+Şimdi isterseniz 10'luk tabandaki bir sayıyı ikilik tabana çevirmek için kullanılan bin() fonksiyonuna bakalım.
+
+bin(4) # bin fonksiyonu . Sayımız 2'lik tabanda yazıldı. (1 ve 0)
+'0b100'
+bin(19) # bin fonksiyonu . Sayımız 2'lik tabanda yazıldı. (1 ve 0)
+'0b10011'
+bin(521) # bin fonksiyonu . Sayımız 2'lik tabanda yazıldı. (1 ve 0)
+'0b1000001001'
+
+
+
+
+"""
