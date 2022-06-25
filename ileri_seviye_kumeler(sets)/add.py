871058363461d8d6c4d16481616985054c8f83f5
Efe Koparal <efekoparal@proton.me>
2022-06-25 19:03:58 +0300

    Create add.py

---
+++ ileri_seviye_kumeler(sets)/add.py
@@ -0,0 +1,13 @@
+"""
+Kümelerin Metodları
+Eleman Eklemek : add() metodu
+Kümeye eleman eklemimizi sağlar. Aynı eleman eklenmeye çalışırsa hata vermez ve herhangi bir ekleme işlemi yapmaz.
+
+x = {1,2,3}
+x.add(4)
+x
+{1, 2, 3, 4}
+x.add(4) # Tekrar eklenmiyor.
+x
+{1, 2, 3, 4}
+"""
