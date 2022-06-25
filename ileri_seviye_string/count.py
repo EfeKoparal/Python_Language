8d969aefdddea0f011c6dc4aea2cb973fd87cfc4
Efe Koparal <efekoparal@proton.me>
2022-06-25 19:02:29 +0300

    Create count.py

---
+++ ileri_seviye_string/count.py
@@ -0,0 +1,15 @@
+"""
+
+count()
+count(x): Stringin içindeki x değerlerini sayar.
+
+count(x,index): Stringin içindeki x değerlerini verilen index değerinden başlayarak saymaya başlar.
+
+"ada kapısı yandan çarklı".count("a")
+6
+"ada kapısı yandan çarklı".count(" ")
+3
+"ada kapısı yandan çarklı".count("a",2)
+5
+
+"""
